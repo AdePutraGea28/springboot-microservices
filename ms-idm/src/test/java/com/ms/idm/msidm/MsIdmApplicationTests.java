51d9bba1762d8fe8049677fa9ca9eb64c5f70fd4
AdePutraGea28 <adeputragea28@gmail.com>
2023-11-18 21:45:54 +0700

    ms-idm project initiate

---
+++ ms-idm/src/test/java/com/ms/idm/msidm/MsIdmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.idm.msidm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsIdmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
